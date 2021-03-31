42e12e9adea2b4d98c416670803cd56aa5806ddc
conordoh98 <cdohert1@tcd.ie>
2021-03-31 19:30:05 +0100

    Update Program.cs
    added function to write stats to a json

--- Server/Program.cs
+++ Server/Program.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
@@ -14,7 +15,7 @@ namespace Server
     {
         public static int current = 0;
         public static int max = 10;
-        static string[] devices = new string[]{"az-c89346883a40"};
+        static string[] devices = new string[] {"az-c89346883a40","AZ-c89346886016"};
         static ThreadStart hostTS = new ThreadStart(host);
         static string[] tempargs;
 
@@ -28,8 +29,9 @@ namespace Server
         {
             tempargs = args;
             Thread hostT = new Thread(hostTS);
-            hostT.Start();
-            await InvokeDeviceMethod.Program.deviceMethod("lock",devices);
+            hostT.Start();  
+            statsToJson();                                                                      //calls function to write stats to .json file
+            await InvokeDeviceMethod.Program.deviceMethod("lock", devices);
             await devicemessages.Event.msg();
         }
 
@@ -41,5 +43,26 @@ namespace Server
                 {
                     webBuilder.UseStartup<Startup>();
                 });
+
+        public static void statsToJson()
+        {
+            DateTime date = DateTime.Now;                                                                                       //gets current date and time in format DD/MM/YYYY HH:MM:SS AM/PM
+            File.WriteAllText(@"C:\Device\server\data.json", "{\"" + date.ToString("yyyy-MM-dd") + "\":[");                     //writes current date to .json file in format YYYY-MM-DD, with some syntax
+            while (Equals(date.ToString("yyyy-MM-dd"), DateTime.Now.ToString("yyyy-MM-dd")))                                    //while still current day
+            {
+                String timeAndCurrent = null;                                                                                   //initialise output string
+                if (Equals(date.ToString("t"), "23:30")){                                                                       //slight change in syntax for final output
+                    timeAndCurrent = "{\"time\":\"" + date.ToString("t") + "\",\n\"value\":" + current + "},]\n";                       
+                }
+                if (Equals((date.ToString("t")[3] + date.ToString("t")[4]), "00") ||                                            //if time ends in 00 or 30 ie outputs time and value for current every half an hour
+                (Equals((date.ToString("t")[3] + date.ToString("t")[4]), "30")))
+                {
+                    timeAndCurrent = "{\"time\":\"" + date.ToString("t") + "\",\n\"value\":" + current + "},\n";                //creates string with time in format HH:MM and value for current
+                }
+                File.WriteAllText(@"C:\Device\server\data.json", timeAndCurrent);                                               //writes output file to .json
+            }
+        }
     }
 }
+
+//String array, store time and current with {"} etc
